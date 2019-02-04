3792f0c063e65f0efe8a01228c8d7da7d2abb6bd
EBaxter1 <ebaxter1@hawk.iit.edu>
2019-02-04 14:30:06 -0600

    Added functions

--- hw2/ebaxter_hw2.py
+++ hw2/ebaxter_hw2.py
@@ -12,22 +12,58 @@ except ValueError:
     print("\nInvaild choice selected, please try again with choice 1-3. Program ending")
     quit()
 
-# variables
-meter = "na"
-velocity = "na"
-pressure = "na"
 
+# functions
+# check meter number 
+def meterCheck (meter):
+    print("\n")
+    if (meter < 50):
+        print("Check the main line and test for pressure")
+        try:
+            pressure = int(input("What did the test show? Enter 1 for Normal, 2 for High or 3 for Low: "))
+            pressureCheck (pressure)
+        except ValueError:
+            print("\nInvaild choice selected, please try again with choice 1-3. Program ending")
+            quit()
+    elif (meter >= 50):
+        print("Measure the flow velcouty at inlet 2-B")
+        try:
+            velocity = int(input("What was the velocity? Enter 1 for Normal, 2 for High or 3 for Low: "))
+            velocityCheck (velocity)
+        except ValueError:
+            print("\nInvaild choice selected, please try again with choice 1-3. Program ending")
+            quit()
 
+# check presssure 
+def pressureCheck (pressure):
+    if (pressure == 1):
+        print("Refer to motor service manual.")
+    elif (pressure == 2 or pressure == 3):
+        print("Refer to main line manual")
+    else:
+        print("\nInvaild choice selected, please try again with choice 1-3. Program ending")
+        quit()
+# check velocity
+def velocityCheck (velocity):
+    if (velocity == 1):
+        print("Refer to inlet service manual.")
+    elif (velocity == 2 or velocity == 3):
+        print("Refer unit for factory service")
+    else:
+        print("\nInvaild choice selected, please try again with choice 1-3. Program ending")
+        quit()
+
+        
 # check first lights 
 if (light == 1):
     print("Do restart procedure and you should be all set!")
-    exit()
 elif (light == 3):
     print("Check fuel line service routine and see what you can find.")
 elif (light == 2):
     print("Shut off all input lines!")
     try:
-        meter = int(input("Next, check meter #3 and enter the number, without digits, that is shown: "))
+        meter = int(input("Next, check meter #3 and enter the number, without decimals, that is shown: "))
+        meterCheck (meter)
     except ValueError:
         print("\nInvaild input, please try again with an integer. Program ending")
         quit()
@@ -35,44 +71,5 @@ else:
     print("Invalid choice entered. Program now ending")
     quit()
 
-print("\n")
-
-
-# check meter
-if (meter < 50):
-    print("Check the main line and test for pressure")
-    try:
-        pressure = int(input("What did the test show? Enter 1 for Normal, 2 for High or 3 for Low: "))
-    except ValueError:
-        print("\nInvaild choice selected, please try again with choice 1-3. Program ending")
-        quit()
-elif (meter >= 50):
-    print("Measure the flow velcouty at inlet 2-B")
-    try:
-        velocity = int(input("What was the velocity? Enter 1 for Normal, 2 for High or 3 for Low: "))
-    except ValueError:
-        print("\nInvaild choice selected, please try again with choice 1-3. Program ending")
-        quit()
-elif (meter == "na"):
-        exit()
-
-# for pressure
-if (pressure == 1):
-    print("Refer to motor service manual.")
-elif (pressure == 2 or pressure == 3):
-    print("Refer to main line manual")
-else:
-    print("\nInvaild choice selected, please try again with choice 1-3. Program ending")
-    quit()
-
-    
-# for velocity
-if (velocity == 1):
-    print("Refer to inlet service manual.")
-elif (velocity == 2 or velocity == 3):
-    print("Refer unit for factory service")
-else:
-    print("\nInvaild choice selected, please try again with choice 1-3. Program ending")
-    quit()
 
 print("I hope I helped! Have a nice day, goodbye!")
