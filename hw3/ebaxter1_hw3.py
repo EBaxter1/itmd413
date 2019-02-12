6873068f7f07d2ae03be75a010600668d9ea2395
EBaxter1 <ebaxter1@hawk.iit.edu>
2019-02-11 21:07:11 -0600

    cleaned up code some and added more comments

--- hw3/ebaxter1_hw3.py
+++ hw3/ebaxter1_hw3.py
@@ -4,57 +4,67 @@ Made By: Erin baxter
 This program will test to see if a card number is vaild or not
 '''
 
-print("Hello and welcome to the card tester program!\n")
+print("Hello and welcome to the credit card tester program!\n")
 
-# get number
+# get number from user and make sure it is an int 
 try:
     cNumber = int(input("Please enter a credit card number to test: "))
-    print("\n")
 except ValueError:
     print("Invaild input, please try again with an integer between 13-16 digits. Program now ending")
     quit()
 
+
+# this function gives user friendly result of if card is valid
+def outputUser(number):
+    if (isValid(number)):
+        print("That credit card number is valid!")
+    else:
+        print("That credit card number is not valid!")
+    print("\nThank you for using this program. See you next time!")
+
+
     
 # return true if the card number is valid
 def isValid(number):
+    # convert number to string and the reverse it 
+    number = str(number)
     reversedNumber = number[::-1]
-    # newNumber = int(reversedNumber)
+    
     length = getSize(reversedNumber)
-    
-    numberList = list(reversedNumber)
-   
-    evenSum = sumOfDoubleEvenPlace(numberList)
-    
-    oddSum = sumOfOddPlace(numberList)
-    
-    totalSum = evenSum + oddSum
-    
-    end = totalSum % 10
-    
-    if (end == 0):
-       print("Number is valid")
-    else:
-        print("Number is not valid")
 
+    #make the number into an array 
+    numberList = list(reversedNumber)
+    
+    totalSum = sumOfDoubleEvenPlace(numberList) + sumOfOddPlace(numberList)
+
+    #check if sum can be divided by 10
+    result = totalSum % 10
+    
+    if (result == 0):
+        return True
+    else:
+        return False 
 
 # get the result from step 2
 def sumOfDoubleEvenPlace(number):
     length = getSize(number)
     evenSum = 0
-    
+
+    #run through card number array and get every even number 
     for x in range(1, length, 2):
         num = getDigit(int(number[x]))
         evenSum+=num
 
-    print(evenSum)
     return evenSum
 
 # Return this number if it is a single digit, otherise, return the sum of the two digits
 def getDigit(number):
     numberProduct = number * 2
+    
     if (numberProduct < 10):
         return numberProduct
     else:
+        # make the product of the number a string and then caculate
         numberProduct = str(numberProduct)
         newList = list(numberProduct)
         newSum = int(newList[0]) + int(newList[1])
@@ -64,11 +74,12 @@ def getDigit(number):
 def sumOfOddPlace(number):
     length = getSize(number)
     oddSum = 0
-    
+
+    #go through each number in array and make it an int
     for x in range(0, length, 2):
         num = int(number[x])
         oddSum+=num
-    print(oddSum)
+ 
     return oddSum
 
 # return true if the digit d is a prefix for number
@@ -77,7 +88,6 @@ def sumOfOddPlace(number):
 
 # return the number of digits in d
 def getSize(d):
-  # reversing card number given by using revered function and making int again
     length = len(d)
     length = int(length)
     return length
@@ -86,11 +96,12 @@ def getSize(d):
 # number of digits in number is less than k, return number
 #def getPrefix(number, k):
 
-# make card number input a string for testing 
-makeString = str(cNumber)
+# make card number input a string fortesting length 
+cardLength = getSize(str(cNumber))
 
-if (len(makeString) >= 13 and len(makeString) <= 16):
-    isValid(makeString)
+# check if real card length and then call first function 
+if (cardLength >= 13 and cardLength <= 16):
+    outputUser(cNumber)
 else:
     print("Number is not between 13-16 digits so it not a card number. Please try again, program now closing")
     quit()
