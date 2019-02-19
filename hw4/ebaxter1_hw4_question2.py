99a06f7ea9b8ad275cee72896cb7d690890b5641
EBaxter1 <ebaxter1@hawk.iit.edu>
2019-02-18 20:12:49 -0600

    was actually able to fix everything, plus fixed error in data

--- hw4/ebaxter1_hw4_question2.py
+++ hw4/ebaxter1_hw4_question2.py
@@ -6,6 +6,8 @@ This program will let a user create a student's grade report
 
 print("Hello and welcome to the Grade Report Generator!\n")
 
+filePath = ("studentReports\\")
+
 try:
     sId = int(input("Enter student id of the student you would like a report for: "))
 except ValueError:
@@ -13,69 +15,88 @@ except ValueError:
     quit()
 
 def main():
-    searchFile()
-
-def searchFile():
     global sId
     stringId = str(sId)
-
+    
     if len(stringId) != 7:
-        print("Not a real id")
-        quit()
+        print("Invaild id number entered. Please enter a 7 digit id number")
+    else:
+        testId = searchFile(stringId)
+        if testId == False:
+            print("Student id not found.")
+        else:
+            fileName = createFile(stringId)
+            reportContent = createReport(testId)
+            writeFile(fileName,reportContent)
+  
+
+def searchFile(studentId):
 
     getFile = open("student_data.txt", "r")
 
     studentInfo = []
 
     for line in getFile:
-       if stringId in line:
+       if studentId in line:
            studentInfo.append(line)
 
+    getFile.close()
+     
     if studentInfo == []:
-        print("Student not found")
-        quit()
-
-    code = []
-    creditAll = []
-    gradeAll = []
-
-    for sClass in studentInfo:
-        stringId, name, courseCode, credit, grade = sClass.split(":")
-        code.append(courseCode)
-        creditAll.append(credit)
-        gradeAll.append(grade[0])
-
-    #test = print("\n".join(map(str,{code}))
+        return False
+    else:
+        return studentInfo
 
 
-    reportFormat = (f'''
-    Student Name: {name}
-    Student ID Number: {stringId}
+def createFile(studentId):
+    global filePath
+    fileName = filePath + studentId + ".txt"
+    return fileName
 
-    Course Code    Course Credits   Course Grade
-    --------------------------------------------
-    ''')
+def createReport(studentInfo):
+    # lists and variables for all the class info
+    allCodes = []
+    allCredits = []
+    allGrades = []
+    stringId = ""
+    name = ""
+    
 
-    length = len(code)
+    #take each part of a single class's info and place them into variables
+    for classInfo in studentInfo:
+        stringId, name, courseCode, credit, grade = classInfo.split(":")
+        allCodes.append(courseCode)
+        allCredits.append(credit)
+        allGrades.append(grade[0])
 
-    str3 = ""
-    str1 = ""
+    # basic format for report 
+    reportFormat = (
+    f'''Student Name: {name}\nStudent ID Number: {stringId}\n\nCourse Code    Course Credits   Course Grade\n--------------------------------------------\n''')
+
+    #get length of how many classes there are for student 
+    length = len(allCodes)
+
+    #add each course and it's info to report 
+    courseStrings = ""
     for x in range(length):
-        str1 = (f'{code[x]}               {creditAll[x]}               {gradeAll[x]}')
-        reportFormat += str1 + "\n    "
+        courseString = (f'{allCodes[x]}                {allCredits[x]}               {allGrades[x]}')
+        reportFormat += courseString + "\n"
         
 
-    print(reportFormat)
+    creditSum = 0
+    product = 0
+    productSum = 0
 
-    sum1 = 0
-    sum2 = 0
-    sum3 = 0
+    #get total course credit hours 
+    for y in allCredits:
+            creditSum += int(y)
 
-    for y in creditAll:
-            sum1 += int(y)
             
-    for w in creditAll:
-        for z in gradeAll:
+
+    #figure out GPA 
+    for w in allCredits:
+        # convert grades to number
+        for z in allGrades:
             if z == 'A':
                 z = 4
             elif z == 'B':
@@ -86,20 +107,43 @@ def searchFile():
                 z = 1
             else:
                 z = 0
-            sum2 = z * int(w)
-        sum3 += sum2
-  
-    sum4 = 0
-    sum4 = sum3 / sum1 
-  
-        
-    total = (f"\n   Total Semester Course Credits Completed: {sum1}")
-    gpa = (f"\n   Semester GPA: {sum4}")
+            # times grade value * that class credit 
+            product = z * int(w)
+        # add up all courses
+        productSum += product
 
-    reportFormat += total
-    reportFormat += gpa 
-    print(reportFormat)
+    # calculate gpa 
+    gpaTotal = 0
+    gpaTotal = productSum / creditSum 
+  
+    # last couple strings of report 
+    totalCredits = (f"\nTotal Semester Course Credits Completed: {creditSum}")
+    calcGPA = (f"\nSemester GPA: {gpaTotal}")
+
+    # add last strings to report
+    reportFormat += totalCredits
+    reportFormat += calcGPA 
       
+    return reportFormat
 
+def writeFile(fName, content):
+    newFile = open(fName, "w+")
+    newFile.write(content)
+    newFile.close()
+
+    print("\nStudent report created! The report was named using the student id\n")
+
+    outputFile(fName)
+
+def outputFile(fPath):
+  print("Your report can be found in the studentReports folder:", fPath)
+  print("An example of your report is as follows:")
+
+  getFile = open(fPath, "r")
+  print(getFile.read())
+  getFile.close()
+    
 
 main()
+
+print("\nProgram Endding, goodbye!")
