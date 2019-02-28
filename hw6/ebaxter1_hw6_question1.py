6c11cc27c303072d519fa12d3302ce13a995eacc
EBaxter1 <ebaxter1@hawk.iit.edu>
2019-02-27 19:13:05 -0600

    Forgot comments

--- hw6/ebaxter1_hw6_question1.py
+++ hw6/ebaxter1_hw6_question1.py
@@ -3,19 +3,22 @@ File name: ebaxter1_hw6_question1.py
 made by: Erin Baxter
 This program creates a pie chart based on expenses
 '''
-
+# import matplot
 import matplotlib.pyplot as plt
 
 print("Hello and welcome to the Pie Chart Generator 11,000!\n")
 
-
+# main method that opens file, reads it and outputs chart 
 def main():
+    #open file 
     print("Opening file...")
     getFile = open("expenses.txt", "r")
 
+    #variables for data 
     cost_labels = []
     costs = []
 
+    # read each line of file and put into variable lists 
     print("Reading file...")
     for line in getFile:
         #print(line)
@@ -23,19 +26,21 @@ def main():
         cost_labels.append(newLabel)
         costs.append(newCost)
 
-        
+    # add data to chart
     print("Creating chart...")
     plt.pie(costs, labels=cost_labels)
 
+    #name chart
     plt.title("Last Month Expenses")
 
+    #display chart 
     print("Displaying chart...")
     plt.show()
-    
+
+    #close file
     print("Closing file...")
     getFile.close()
-        
-
+    
 
 #call main
 main()
