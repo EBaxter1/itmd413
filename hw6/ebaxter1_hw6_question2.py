ceedb24fb70b5fa683beb664d26b3deb2ef8d3f8
EBaxter1 <ebaxter1@hawk.iit.edu>
2019-02-27 20:24:19 -0600

    Finished hw6

--- hw6/ebaxter1_hw6_question2.py
+++ hw6/ebaxter1_hw6_question2.py
@@ -8,6 +8,7 @@ import matplotlib.pyplot as plt
 
 print("Hello and welcome to the Gas Averages Data Displayer 12,000!\n")
 
+#main function that opens file, reads file and then sends data to other functions
 def main():
     #open file 
     print("Opening file...")
@@ -18,16 +19,19 @@ def main():
     totalWeeks = []
     i = 1
 
+    #read file
     print("Reading file...")
     for line in getFile:
         allGas.append(float(line.strip()))
         totalWeeks.append(i)
         i += 1
 
+    #close file
     print("Closing file...")
-    
-    graphType = input("\nWould you like the data as a bar chart or a line graph?(Enter bar or line): ")
+    getFile.close()
 
+    #pick type of graph
+    graphType = input("\nWould you like the data as a bar chart or a line graph?(Enter bar or line): ")
     graphType = graphType.lower().strip()
 
     if graphType == "line":
@@ -37,32 +41,31 @@ def main():
     else:
         print("\nInvaild input. Please try again and enter 'line' or 'bar'.")
 
-
+#this function will make a line graph based on the data from file 
 def makeLine(x,y):
+    #make graph 
     print("\nCreating Graph...")
     plt.plot(x,y)
-
     plt.title("Average Gas Prices per Week for 1994", color="purple")
-
     plt.xlabel("Week Number", color='red')
     plt.ylabel("Average Gas Price", color='red')
-
     plt.grid(True)
 
+    #display graph
     print("Displaying Graph...")
     plt.show()
-    
+
+#this function will make a bar chart based on the data from file 
 def makeBar(x,y):
+    #make graph 
     print("\nCreating Graph...")
     plt.bar(x,y, color="green")
-
     plt.title("Average Gas Prices per Week for 1994", color="purple")
-
     plt.xlabel("Week Number", color='red')
     plt.ylabel("Average Gas Price", color='red')
-
     plt.grid(True)
 
+    #display graph
     print("Displaying Graph...")
     plt.show()
 
