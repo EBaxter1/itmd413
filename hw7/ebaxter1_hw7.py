915213d448079be2a1ff823cc6eace47ebee57a0
EBaxter1 <ebaxter1@hawk.iit.edu>
2019-03-11 17:45:21 -0500

    Starting homework 7

---
+++ hw7/ebaxter1_hw7.py
@@ -0,0 +1,7 @@
+'''
+File Name: ebaxter1_hw7.py
+Made by: Erin Baxter
+This program will take a phone bumber that has letters and make it all numbers
+'''
+
+print("Hello and welcome to the Number Converter 13,000!\n")
