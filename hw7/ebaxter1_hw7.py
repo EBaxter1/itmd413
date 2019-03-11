5f675ca886c89859a8321c49fd1c2bd7f486b069
EBaxter1 <ebaxter1@hawk.iit.edu>
2019-03-11 17:48:27 -0500

    Didn't get far before break :D

--- hw7/ebaxter1_hw7.py
+++ hw7/ebaxter1_hw7.py
@@ -5,3 +5,15 @@ This program will take a phone bumber that has letters and make it all numbers
 '''
 
 print("Hello and welcome to the Number Converter 13,000!\n")
+
+def main():
+
+def testNum(num):
+    
+
+def convertNumber(num):
+
+#call main
+main()
+
+print("\nHave a nice day! Program now ending")
