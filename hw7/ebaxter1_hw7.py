a6cfdb4813695eee5d9e0cfeed8f8b82b5d1eebe
EBaxter1 <ebaxter1@hawk.iit.edu>
2019-03-11 20:58:43 -0500

    finished hw7

--- hw7/ebaxter1_hw7.py
+++ hw7/ebaxter1_hw7.py
@@ -1,7 +1,7 @@
 '''
 File Name: ebaxter1_hw7.py
 Made by: Erin Baxter
-This program will take a phone bumber that has letters and make it all numbers
+This program will take a phone number that has letters and make it all numbers
 '''
 
 #import regualr expressions 
