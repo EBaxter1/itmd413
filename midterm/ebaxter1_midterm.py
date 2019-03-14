8877b7dd7900c2ec240540e0889be6fe1dc49bc7
EBaxter1 <ebaxter1@hawk.iit.edu>
2019-03-13 21:06:33 -0500

    still working hit a bump

--- midterm/ebaxter1_midterm.py
+++ midterm/ebaxter1_midterm.py
@@ -12,8 +12,7 @@ def main():
     annualRate = input("Please enter annual interest rate: ")
 
     if testInput(loanAmount, yearAmount, annualRate):
-        print("made it")
-        #calcPayment()
+        calcPayment(loanAmount, yearAmount, annualRate)
 
 def testInput(loan, years, rate):
     if " " in loan:
@@ -26,20 +25,33 @@ def testInput(loan, years, rate):
         print("No input found for one option. Please try again!")
     elif loan.isnumeric() == False:
         print("Loan ammount entered is not a number! Please try again")
+    elif "." in years:
+        print("Input error! Years amount can not have a decimal! Please again")
     elif years.isnumeric() == False:
         print("Year ammount entered is not a number! Please try again")
     elif rate.isnumeric() == False:
         print("Annual rate entered is not a number! Please try again!")
-    elif float(years):
-        print("Input error! Years amount can not have a decimal! Please again")
     elif float(loan) and int(years) and float(rate):
         return True
     else:
         print("Invaild input. Please try again")
         
+def calcPayment(loan, years, rate):
+    convertRate = float(rate) / 100
+    months = float(years) * 12
+    monthlyPay = float(loan) / months
+    interst = convertRate * monthlyPay
+   
+    print("{0:.2f}".format(interst), convertRate,"{0:.2f}".format(monthlyPay))
+    stringInterst = "{0:.2f}".format(interst)
+    stringMonthly = "{0:.2f}".format(monthlyPay)
+    i = 1
+    print(f'''Monthly Payment: {monthlyPay}\nTotal Payment: {loan}\n\nPayment#    Interest   Principal   Balance''')
+    for x in range(1,int(months)):
+        print(f'{x}   ' + format(interst, '{0:.2f}.') + '   {0:.2f}'.format(monthlyPay - interst))
         
-
-#def calcPayment():
+        
+        
 
 #def displayInfo():
     
