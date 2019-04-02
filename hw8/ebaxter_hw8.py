8a0c23677ff94d283949cfb71ee79e32d421776e
EBaxter1 <ebaxter1@hawk.iit.edu>
2019-04-01 19:29:37 -0500

    Still working........ :(

--- hw8/ebaxter_hw8.py
+++ hw8/ebaxter_hw8.py
@@ -4,33 +4,70 @@ Made by: Erin Baxter
 This program save user data as dictionary and then output it to a file.
 '''
 
-print("Hello and welcome to the Email Dictionary Generator 18,000!\n")
+#import regularr expressions
+import re
+import json
+
+print("Hello and welcome to the Email Dictionary Generator 18,000!")
 
 def main():
-      # vairbale to to program running
-      noStop = "y"
-
-      while noStop == "y":
-          try:
-              menuOption = int(input(f'''What would you like to do?\n1. Look up email address(enter 1)\n2. Add new name and email address(enter 2)\n3. Update existing email address(enter 3)\n4. Delete existing name and email address(enter 4)\n5. Exit program (enter any key other then 1-4)\nEnter option: '''))
-              if choice == 1:
-                lookUpValue()
-              elif choice == 2:
-                addValue()
-              elif choice == 3:
+    # vairbale to to program running
+    dic = {}
+    noStop = "y"
+    fileName = ("userEmails.txt")
+      
+    while noStop == "y":
+        try:
+            menuOption = int(input(f'''\nWhat would you like to do?\n1. Look up email address(enter 1)\n2. Add new name and email address(enter 2)\n3. Update existing email address(enter 3)\n4. Delete existing name and email address(enter 4)\n5. Exit program (enter any key other then 1-4)\nEnter option: '''))
+            if menuOption == 1:
+                lookUpValue(fileName)
+            elif menuOption == 2:
+                addValue(dic, fileName)
+            elif menuOption == 3:
                 updateValue()
-              elif choice == 4:
+            elif menuOption == 4:
                 delValue()
-          except ValueError:
-              #writeValue()
-              noStop = "n"
+            else:
+                #writeValue()
+                file.close()
+                noStop = "n"
+        except ValueError:
+            #writeValue()
+            noStop = "n"
                       
-          
+
+def lookUpValue(file):
+    nfile = open(file, "r")
+    '''email = input("Please enter the email address to search for: ")
+    realEmail = r"^\w+@+\w+\.\w"
+    if re.match(realEmail, email):
+        for line in file:
+            if email 
+    else:
+        print("Invalid email format! Please try again real email format(contains @ and .)")'''
+    #print(file.read())
+    d = {1:2}
+    for line in nfile:
+        #print(line)
+        d.update(json.loads(line))
+    print(d)
+    
+
+def addValue(stuff,file):
+    file = open(file, "a+")
+    '''name = input("Please enter new name: ")
+    email = input("Please enter new eamil: ")
+    newData = {name:email}
+    stuff.update(newData)'''
+    stuff.update({'a':'a1', 'b':'b2'})
+    print(stuff)
+    length = len(stuff)
+    for x in stuff.items():
+        file.write(str(x) + "\n")
+    
+    
+    
 '''
-def lookUpValue():
-
-def addValue():
-
 def updateValue():
 
 def delValue():
