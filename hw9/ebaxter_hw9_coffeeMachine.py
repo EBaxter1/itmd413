045a557c214ae9219700c2d168436bb4223dcd7e
EBaxter1 <ebaxter1@hawk.iit.edu>
2019-04-08 20:24:56 -0500

    Finishing up

--- hw9/ebaxter_hw9_coffeeMachine.py
+++ hw9/ebaxter_hw9_coffeeMachine.py
@@ -4,26 +4,31 @@ Made by: Erin Baxter
 This program will have the class for a coffee machine and the methods needed to interact with it
 '''
 
+# class that has methods to get information about a coffeeMachine 
 class coffeeMachine:
-    #stock machine with cups and set price
-    def __init__(self,quantity = 10, price = 1.30, total = 0.0):
+    #constructor for number of cups, price of coffee and total amount of moeny inserted by user
+    def __init__(self,quantity = 0, price = 1.30, total = 0.0):
         self.price = price
         self.quantity = quantity
         self.total = total
 
-    # display the quantity and price of coffee
+    # get the current quantity of coffee
     def getQuantity(self):
         return self.quantity
 
+    # get the current price of coffee
     def getPrice(self):
         return self.price
-
+    
+    # get the total amount of user money 
     def getTotal(self):
         return self.total
 
+    # set current quantity 
     def setQuantity(self, quantity):
         self.quantity = quantity
 
+    #set current user total entered money
     def setTotal(self, total):
         self.total = total
 
--- hw9/ebaxter_hw9_testCoffeeMachine.py
+++ hw9/ebaxter_hw9_testCoffeeMachine.py
@@ -3,22 +3,44 @@ File Name: ebaxter_hw9_testCoffeeMachine.py
 Made by: Erin Baxter
 This program will have the constructors to test the methods in coffeeMachine
 '''
+# get class information from other file
 from ebaxter_hw9_coffeeMachine import coffeeMachine
+#make a global variable for bew object
 newMachine = coffeeMachine()
 
+#main method gives user menu options for what they want to do
 def main():
-    amount, price = menu()
-    print(f"The quantity of cups currently in the machine is: {amount} and the price per cup is: " + "${:.2f}".format(price))
-    
-    try:
-        quart = int(input("Please enter the number of quarters you would like to insert: "))
-        dime = int(input("Please enter the number of dimes you would like to insert: "))
-        nick = int(input("Please enter the number of nickeles you would like to insert: "))
+    noStop = "y"
 
-        money = insert(quart, dime, nick)
-        select()
-    except ValueError:
-        print("Error! Please enter a whole number!")
+    while noStop == "y":
+        try:
+            menuOption = int(input(f'''\nWhat would you like to do?\n1. Check current inverntory and price(enter 1)\n2. Insert coins(enter 2)\n3. Select coffee(enter 3)\n4. Refund money(enter 4)\n5. Exit program (enter any key other then 1-4)\nEnter option: '''))
+            if menuOption == 1:
+                amount, price = menu()
+                print(f"\nThe quantity of cups currently in the machine is {amount} and the price per cup is: " + "${:.2f}".format(price))
+            elif menuOption == 2:
+                if newMachine.getQuantity() == 0:
+                    print("\nCurrent inventory is 0. Coffee can not be made. please try again later")
+                else:
+                    try:
+                        quart = int(input("\nPlease enter the number of quarters you would like to insert: "))
+                        dime = int(input("Please enter the number of dimes you would like to insert: "))
+                        nick = int(input("Please enter the number of nickeles you would like to insert: "))
+
+                        money = insert(quart, dime, nick)
+                    except ValueError:
+                        print("Error! Please enter a whole number!")
+            elif menuOption == 3:
+                select()
+            elif menuOption == 4:
+                refund()
+            else:
+                noStop = "n"
+        except ValueError:
+            noStop = "n"
+
+       
+   
 
 def menu():
     quantity = newMachine.getQuantity()
@@ -68,11 +90,13 @@ def select():
     print("\nCurrent Change Inserted: " + "${:.2f}".format(newTotal))
     if inventory == 0:
         print("Current inventory is 0. Coffee can not be made. please try again later")
-        refund()
+        if newTotal > 0:
+            refund()
+    elif newTotal == 0:
+        print("Please insert coins. Price per cup is " + "${:.2f}".format(singlePrice))
     else:
         try:
             amount = int(input("How many cups of coffee do you want?: "))
-            
             if (newTotal / amount) == singlePrice and amount <= inventory:
                 newMachine.setQuantity(inventory - amount)
                 newMachine.setTotal(0.0)
@@ -95,14 +119,24 @@ def select():
 
 def refund():
     returnTotal = newMachine.getTotal()
-    coin = ''' ___
-              /   \
-              \___/
-    print("\nReturning " + "${:.2f}".format(returnTotal) + "...")
-    newMachine.setTotal(0.0)
-    print("Thank you come again!")
+    coin = '''______________
+              \  ___
+               \/   \    
+                \\___/
+                 \     ___      ___
+                  \   /   \    /   \   |
+                   \__\___/____\___/___|'''
+
+    if returnTotal > 0:
+        print("\nReturning " + "${:.2f}".format(returnTotal) + "...")
+        print(coin)
+        newMachine.setTotal(0.0)
+    else:
+        print("\nNo change available!")
     
 
     
 # call main method
 main()
+
+print("\nThank you come again!")
