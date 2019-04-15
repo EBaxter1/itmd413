3c50aa6f6f41d559784dc78a102a7626b1769036
EBaxter1 <ebaxter1@hawk.iit.edu>
2019-04-15 13:17:26 -0500

    finishing up homework 10

--- hw10/ebaxter_hw10_inheritance.py
+++ hw10/ebaxter_hw10_inheritance.py
@@ -4,39 +4,54 @@ Made by: Erin Baxter
 This program will have the classes Employee and ProductionWorker and their attributes 
 '''
 
+#parent class that has employee name and number, can that print info
 class Employee(object):
+    #constructor for name and number
     def __init__(self, name, eNumber):
         self.name = name
         self.eNumber = eNumber
 
+    # method to display information 
     def displayData(self):
-        print(self.name)
-        print(self.eNumber)
+        print("Employee name:", self.name)
+        print("Employee Number:", self.eNumber)
 
+# child class that takes name and number from parent and also makes shift and rate 
 class ProductionWorker(Employee):
-
+    # constructor for name, number, shift and rate for a worker
     def __init__(self, name, eNumber, shift, rate):
+        # constructor to get __init__ from parent class
         super(ProductionWorker, self).__init__(name, eNumber)
         self.shift = shift
         self.rate = rate
+        
+    # method to display information 
+    def displayOtherData(self):
+        print("Worker shift:", self.shift)
+        print("Hourly pay rate:", "${:.2f}".format(self.rate))
 
-       
+    # method to get current shirt for worker
     def getShift(self):
         return self.shift
 
+    # method to get current pay rate for worker 
     def getRate(self):
         return self.rate
-
-    def setShift(self):
+    
+    # method to set new shift for worker 
+    def setShift(self, shift):
         self.shift = shift
 
-    def setRate(self):
+    # method to set new pay rate for worker
+    def setRate(self, rate):
         self.rate = rate
 
-    def setName(self):
+    # method to set new name of worker 
+    def setName(self, name):
         self.name = name
 
-    def setNumber(self):
+    # method to set new number of worker
+    def setNumber(self, eNumber):
         self.eNumber = eNumber
 
     
--- hw10/ebaxter_hw10_testInheritance.py
+++ hw10/ebaxter_hw10_testInheritance.py
@@ -1,15 +1,59 @@
 '''
 File Name: ebaxter_hw10_testInheritance.py
 Made by: Erin Baxter
-This program will test the Employee and ProductionWorker classes by interacting with them
+This program will test the Employee and ProductionWorker classes by interacting with them using user input
 '''
-
 # get class information from other file
-# from ebaxter_hw10_inheritance import Employee
 from ebaxter_hw10_inheritance import ProductionWorker
-# global variable of new object from class
 
-newWorker = ProductionWorker("Ana", 1198, 1, 12)
+# make new global object of class 
+newWorker = ProductionWorker("", 0, 0, 0)
 
-print(newWorker.getShift())
-newWorker.displayData()
+print("Hello and welcome to the New Worker Generator 20,000!")
+
+# main method gets user input and calls function to set input and then displays input
+def main():
+    # variable to run while loop
+    noStop = "y"
+
+    # while loop to keep it running if there is an error
+    while noStop == "y":
+        # get user input
+        newName = input("\nPlease enter new worker's name: ")
+        try:  
+            newId = int(input("Please enter new id number using only whole numbers: "))
+            newShift = int(input("Please enter shift for new worker(1 for day shift, 2 for night shift): "))
+            if newShift > 2 or newShift < 1: # make sure only day or night shift 
+                print("Error, Shift can only be 1 or 2! Please try again")
+                continue # make loop start over if error 
+        except ValueError:
+            print("Error! Please enter a whole number!")
+            continue # make loop start over if error 
+        newRate =  input("Please enter hourly pay rate: ")
+        try:
+            newRate = float(newRate) # check and make sure input is a number and convert if it is
+            # call function to add input to created objects 
+            createPerson(newName, newId, newShift, newRate)
+            
+            # output data
+            print("\nNew Worker Created! Information is as follows:")
+            newWorker.displayData() # call method from parent class
+            newWorker.displayOtherData() # call method from child class
+            break # end while loop
+        except ValueError:
+            print("Error! Please enter a number for rate!")
+            continue # make loop start over if error 
+                    
+                       
+# this function updates the created object attributes with new worker information
+def createPerson(name, Id, shift, rate):
+    newWorker.setName(name) # update name attibute ob object 
+    newWorker.setNumber(Id) # update number attribute of object
+    newWorker.setShift(shift) # update shift attribute of object
+    newWorker.setRate(rate) # update rate attribute of object
+
+
+# call main method
+main()
+
+print("\nThank you. Goodbye!")
