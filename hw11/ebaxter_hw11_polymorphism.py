26dd681d11b1a8b9ff931ff487f6a99a72897a3f
EBaxter1 <ebaxter1@hawk.iit.edu>
2019-04-22 11:54:00 -0500

    Starting homework 11

---
+++ hw11/ebaxter_hw11_polymorphism.py
@@ -0,0 +1,5 @@
+'''
+File Name: ebaxter_hw11_polymorphism.py
+Made by: Erin Baxter
+This program will have the classes Ship, CruiseShip, CargoShip and their members
+'''
---
+++ hw11/ebaxter_hw11_testPolymorphism.py
@@ -0,0 +1,5 @@
+'''
+File Name: ebaxter_hw11_testPolymorphism.py
+Made by: Erin Baxter
+This program will go through the classes created in th epolumorphism file and call the print function and test them using issinstance
+'''
