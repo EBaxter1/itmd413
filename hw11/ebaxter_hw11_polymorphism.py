371b52e0e38ade21c8966847d34b8b5b3033672d
EBaxter1 <ebaxter1@hawk.iit.edu>
2019-04-22 17:38:14 -0500

    little updates to fix spacing and added comments

--- hw11/ebaxter_hw11_polymorphism.py
+++ hw11/ebaxter_hw11_polymorphism.py
@@ -1,59 +1,74 @@
 '''
 File Name: ebaxter_hw11_polymorphism.py
 Made by: Erin Baxter
-This program will have the classes Ship, CruiseShip, CargoShip and their members
+This program will have the classes Ship, CruiseShip, CargoShip and their members and a print function that will be overrated 
 '''
 
 # parent class that has name of ship, built year, accessers, mutators and print function
 class Ship(object):
+    # constuctor
     def __init__(self, name, year):
+        #member variables 
         self.name = name
         self.year = year
 
+    # getters 
     def getName(self):
         return self.__name
 
     def getYear(self):
         return self.year
 
+    # setters 
     def setName(self, name):
         self.__name = name
 
     def setYear(self, year):
         self.year = year
-        
+
+    # print function  
     def displayData(self):
         print("Ship name:", self.name)
         print("Ship built in:", self.year)
 
-
+# child class that has name of ship, build year and number of passangers it can hold 
 class CruiseShip(Ship):
+    # constuctor 
     def __init__(self, name, year, peeps):
+        # get memebr variables from parent class 
         super(CruiseShip, self).__init__(name, year)
-        self.peeps = peeps
+        self.peeps = peeps # member variable 
 
+    # getter 
     def getPeeps(self):
         return self.peeps
 
+    #setter
     def setPeeps(self, peeps):
         self.peeps = peeps
 
+    # print function
     def displayData(self):
         print("Ship name:", self.name)
         print("Maximum number of passengers:", self.peeps)
 
-
+# child class that has name of ship, build year and cargo capacity 
 class CargoShip(Ship):
+    # constructor
     def __init__(self, name, year, cargo):
+        # get memebr variables from parent class 
         super(CargoShip, self).__init__(name, year)
-        self.cargo = cargo
+        self.cargo = cargo # memeber variable 
 
+    # getter 
     def getCargo(self):
         return self.cargo
 
+    # setter
     def setCargo(self, peeps):
         self.cargo = cargo
 
+    # print function 
     def displayData(self):
         print("Ship name:", self.name)
         print(f"Ship cargo capacity: {self.cargo} tons")
--- hw11/ebaxter_hw11_testPolymorphism.py
+++ hw11/ebaxter_hw11_testPolymorphism.py
@@ -10,6 +10,7 @@ from ebaxter_hw11_polymorphism import CargoShip
 
 print("Hello and welcome to the Ship info displayer 25,000!\n")
 
+# main function that has a remade list of ships and a menu which items will call different functions 
 def main():
     #make some different ship objects
     newShipOne = Ship("Sally", 1917)
@@ -19,70 +20,82 @@ def main():
     #make list of ship objects
     allShips = [newShipOne, newShipTwo, newShipThree]
 
-    
-    while 1 == 1:
+    # while loop for menu
+    while 1 == 1: # since using break and continue can make loop just something always true
+        # try catch for non ints
         try:
             menuOption = int(input(f'''What would you like to do?\n1. List current ships(enter 1)\n2. Add new ship(enter 2)\n3. Exit program (enter any key other then 1 or 2)\nEnter option: '''))
+            # option 1
             if menuOption == 1:
+                # calls function and passes list
                 displayInfo(allShips)
-                continue
+                continue # keeps loop going after function returns 
             elif menuOption == 2:
+                # calls function and updates list with returned value
                 allShips = addShip(allShips)
-                continue
+                continue # keeps loop going after funciton returns
             else:
-                break
+                break # ends loop and exits main
         except ValueError:
-            break
-
-    # call function to display all the ship information
+            break # ends loop and exits main
     
-
+# this function will create a new ship object and add it to the ships list
 def addShip(currentList):
-    while 1 == 1:
+    # while loop for creation 
+    while 1 == 1: # since using break and continue can make loop just something always true
+        # try catch for options
         try:
-            shipType = int(input("What kind of ship is it? 1)Normal Ship, 2)Cruise Ship, 3)Cargo Ship: "))
+            #get ship type to know what type of object 
+            shipType = int(input("\nWhat kind of ship is it? 1)Normal Ship, 2)Cruise Ship, 3)Cargo Ship: "))
             shipName = input("What is the name of the ship? ")
-            try:
+            try: # try for numbers
                 shipYear = int(input("What year was the ship made?: "))
-                if shipYear < 1000 or shipYear > 9999:
+                if shipYear < 1000 or shipYear > 9999: # make sure valid year 
                     print("Error! Please enter a 4 digit year!")
                     continue
                 if shipType == 1:
-                    newItem = Ship(shipName, shipYear)
-                    break
+                    # makes new Ship object 
+                    newItem = Ship(shipName, shipYear) 
+                    break # ends loop
                 elif shipType == 2:
+                    # extra information for Cruise Ship object 
                     shipSize = int(input("What is the max number of passengers for this ship?: "))
                     newItem = CruiseShip(shipName, shipYear, shipSize)
-                    break
+                    break # ends loop 
                 elif shipType == 3:
+                    # extra info for Cargo Ship object 
                     shipCargo = float(input("What is the ship's cargo capacity in tons?: "))
                     newItem = CargoShip(shipName, shipYear, shipCargo)                 
-                    break
+                    break # end loop 
             except ValueError:
-                print("Error! Please enter a number!")
-                continue       
+                print("Error! Please enter a whole number!")
+                continue # make loop keep going for mistakes    
         except ValueError:
             print("Error! Please enter either 1, 2 or 3")
-            continue
-                        
-    currentList.append(newItem)
+            continue # keep it going s
 
-    print("\n")
+    # add need object to list
+    currentList.append(newItem) 
+    print("New ship element has successfully been created!\n")
 
+    #return updated list 
     return currentList
-    
-def displayInfo(newList):
-    print("\n")
-    for ship in newList:
-        ship.displayData()
-        if isinstance(ship, CruiseShip):
-            print("This here is a Cruise ship!")
-        elif isinstance(ship, CargoShip):
-            print("This here is a Cargo ship!")
-        elif isinstance(ship, Ship):
-            print("This here is just a normal ship!")
-        print("\n")
 
+# this function will display the print function of all the ship objects and test what type they are  
+def displayInfo(newList):
+    print("\nCurrently stored ships:")
+    # for loop to go through all objects
+    for ship in newList:
+        #call function 
+        ship.displayData()
+        #checks each object to see what kind of ship class it was made from
+        if isinstance(ship, CruiseShip):
+            print("This here is a Cruise ship!\n")
+        elif isinstance(ship, CargoShip):
+            print("This here is a Cargo ship!\n")
+        elif isinstance(ship, Ship):
+            print("This here is just a normal ship!\n")
+   
 # call main
 main()
 
