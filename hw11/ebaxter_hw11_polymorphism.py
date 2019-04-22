a0660e56403c75628b46ecd1d74908f198a4c3bd
EBaxter1 <ebaxter1@hawk.iit.edu>
2019-04-22 13:07:09 -0500

    commiting before I lose wifi

--- hw11/ebaxter_hw11_polymorphism.py
+++ hw11/ebaxter_hw11_polymorphism.py
@@ -3,3 +3,48 @@ File Name: ebaxter_hw11_polymorphism.py
 Made by: Erin Baxter
 This program will have the classes Ship, CruiseShip, CargoShip and their members
 '''
+
+# parent class that has name of ship, built year, accessers, mutators and print function
+class Ship(object):
+    def __init__(self, name, year):
+        self.__name = name
+        self.__year = year
+
+    def getName(self):
+        return self.name
+
+    def getYear(self):
+        return self.year
+
+    def setName(self, name):
+        self.name = name
+
+    def setYear(self, year):
+        self.year = year
+        
+    # @property
+    def displayData(self):
+        print("Ship name:", self.__name)
+        print("Ship built in:", self.__year)
+
+
+def CruiseShip(Ship):
+    def __init__(self, peeps):
+        Ship.__init__(self, name)
+        self.peeps = peeps
+
+    def getPeeps(self):
+        return self.peeps
+
+    def setPeeps(self, peeps):
+        self.peeps = peeps
+
+    # @property
+    def displayOData(self):
+        print("Ship name:", self.name)
+        print("Maximum number of passengers:", self.peeps)
+
+
+        
+
+
--- hw11/ebaxter_hw11_testPolymorphism.py
+++ hw11/ebaxter_hw11_testPolymorphism.py
@@ -3,3 +3,20 @@ File Name: ebaxter_hw11_testPolymorphism.py
 Made by: Erin Baxter
 This program will go through the classes created in th epolumorphism file and call the print function and test them using issinstance
 '''
+
+from ebaxter_hw11_polymorphism import Ship
+from ebaxter_hw11_polymorphism import CruiseShip
+
+'''
+for a in [Ship("Blue", 1917), CruiseShip(35)]:
+    a.displayData()
+'''
+
+
+b = Ship("Blue", 1917)
+b.displayData()
+
+
+a = CruiseShip(35)
+a.displayOData()
+
