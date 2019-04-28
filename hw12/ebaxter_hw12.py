76340d9a0508b2da9758b959f52783bcdcd03ac8
EBaxter1 <ebaxter1@hawk.iit.edu>
2019-04-28 17:38:42 -0500

    finishing up homework 12

--- hw12/ebaxter_hw12.py
+++ hw12/ebaxter_hw12.py
@@ -4,28 +4,69 @@ Made by: Erin Baxter
 This program has a digits reverse function and a decimal to hex function
 '''
 
-print("Hello and welcome to the Recursion Fun Maker 26,000!\n")
+print("Hello and welcome to the Recursion Fun Maker 26,000!")
 
 def main():
-    number = int(input("Please enter a number to test: "))
+    while 1 == 1:
+        try:
+            menuOption = int(input(f'''\nWhat would you like to do?\n1. Reverse a number\n2. Convert a decimal to a hex number\n3. End program\nEnter option: '''))
+            if menuOption == 1:
+                try:
+                    number = int(input("\nPlease enter a number to reverse: "))
+                    reverseDisplay(number)
+                    continue
+                except ValueError:
+                    print("Error! Please enter a integer!")
+                    continue
+            elif menuOption == 2:
+                try:
+                    number = int(input("\nPlease enter a decimal number to convert: "))
+                    decimalToHex(number)
+                    continue
+                except ValueError:
+                    print("Error! Please enter a integer!")
+                    continue   
+            else:
+                break
+        except ValueError:
+            break
+                
 
-    reverseDisplay(number, 4)
 
-def reverseDisplay(value, count):
-   getLength = len(str(value))
-   newList = list(str(value))
-   num = newList.pop()
-   newList.insert((getLength - 1), num)
-   print(newList)
-   value = int("".join(newList))
-   print(value)
-   if count > 0:
-       reverseDisplay(value, (count - 1))
+def reverseDisplay(value):
+    test = type(value)
+    if test is int:
+        value = list(str(value))
+        print("The reverse of that number is: ", end="")
+    if len(value) > 0:
+        num = value.pop()
+        print(int(num), end="")
+        reverseDisplay(value)
+    else:
+        print()
+  
+    
 
-#def decimalToHex(value):
+def decimalToHex(value):
+    decimalToHexHelper(value, "")
+ 
+def decimalToHexHelper(value, valueString):
+    hexList = ["A" , "B", "C", "D", "E", "F"]
+    if value > 0:
+        dec = value % 16
+        if dec > 9:
+            dec = hexList[dec - 10]
+        else:
+            dec = str(dec)
+        valueString = dec + valueString 
+        value = value / 16
+        decimalToHexHelper(int(value), valueString)
+    else:
+        print("That number in hex is:", valueString)    
+      
 
 
 # call main 
 main()
 
-print("Goodbye!")
+print("\nGoodbye!")
